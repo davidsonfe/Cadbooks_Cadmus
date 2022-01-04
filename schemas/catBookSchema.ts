c725766cee41e07c56aceb2a3962f4b333d5a1cf
jp <joaoprsilva@fac.pe.senac.br>
2022-01-04 12:31:06 -0300

    Categoria de Livros BugFixes

--- schemas/catBookSchema.ts
+++ schemas/catBookSchema.ts
@@ -31,4 +31,81 @@ const catBookBodySchema = {
   }
 };
 
-module.exports = {catBookBodySchema};
+const catBookAllSchema = {
+  response: {
+    200: {
+      type: 'array',
+      properties: {
+        cat_id: {type: "string"},
+        desc_cat: {type: "string"},
+        dias_limite: {type: "number"},
+        multa: {type: "number"}
+      },
+    },
+    400: {
+      type: 'object',
+      properties: {
+        msg: {type: "string"},
+      },
+    },
+    404: {
+      type: 'object',
+      properties: {
+        msg: {type: "string"},
+      },
+    }
+  }
+};
+
+const catBookOneSchema = {
+  required: ["cat_id"],
+  response: {
+    200: {
+      type: 'array',
+      properties: {
+        cat_id: {type: "string"},
+        desc_cat: {type: "string"},
+        dias_limite: {type: "number"},
+        multa: {type: "number"}
+      },
+    },
+    400: {
+      type: 'object',
+      properties: {
+        msg: {type: "string"},
+      },
+    },
+    404: {
+      type: 'object',
+      properties: {
+        msg: {type: "string"},
+      },
+    }
+  }
+};
+
+const catBookUpDelSchema = {
+  required: ["cat_id"],
+  response: {
+    200: {
+      type: 'object',
+      properties: {
+        msg: {type: "string"},
+      },
+    },
+    400: {
+      type: 'object',
+      properties: {
+        msg: {type: "string"},
+      },
+    },
+    404: {
+      type: 'object',
+      properties: {
+        msg: {type: "string"},
+      },
+    }
+  }
+};
+
+module.exports = {catBookBodySchema, catBookAllSchema, catBookOneSchema, catBookUpDelSchema};
