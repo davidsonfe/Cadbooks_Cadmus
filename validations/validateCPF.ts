6b6d2f196e3666eb265b09cd42aa235e0fa0c276
jp <joaoprsilva@fac.pe.senac.br>
2022-01-06 14:14:15 -0300

    validate

--- validations/validateCPF.ts
+++ validations/validateCPF.ts
@@ -3,13 +3,19 @@ export const validateCPF = (cpf: any) => {
   if (!!cpf.match(/(\d)\1{10}/))
     return false;
   cpf = cpf.split('');
-  const validDigito = cpf.filter((digit: any, index: any, array: any) => index >= array.length - 2 && digit).map( (el: any) => {
+  const validDigit = cpf.filter((digit: any, index: any, array: any) => {
+    return index >= array.length - 2 && digit;
+  }).map( (el: any) => {
     return +el;
   });
-  const validar = (pop: any) => cpf.filter((digit: any, index: any, array: any) => index < array.length - pop && digit).map((el: any) => {
+  const validate = (pop: any) => cpf.filter((digit: any, index: any, array: any) => {
+    return index < array.length - pop && digit;
+  }).map((el: any) => {
     return +el;
   });
-  const resto = (count: any, pop: any) => (validar(pop).reduce((soma: any, el: any, i: any) => soma + el * (count - i), 0) * 10) % 11 % 10;
+  const rest = (count: any, pop: any) => {
+    return (validate(pop).reduce((soma: any, el: any, i: any) => soma + el * (count - i), 0) * 10) % 11 % 10;
+  };
 
-  return !(resto(10,2) !== validDigito[0] || resto(11,1) !== validDigito[1]);
+  return !(rest(10,2) !== validDigit[0] || rest(11,1) !== validDigit[1]);
 }
