f14866d8a3a8e414bd2f47a9f022bc5402e16a2b
jp <joaoprsilva@fac.pe.senac.br>
2022-01-07 01:57:28 -0300

    t

--- routes/devolutionRoutes.ts
+++ routes/devolutionRoutes.ts
@@ -14,7 +14,7 @@ export async function devlutionRoutes(fastify: typeof server) {
     },
     async (request: any, reply: any) => {
       try {
-        const penalty = await devolution.postDevolution(request.body.isn_id_cop, request.body as DevolutionModel);
+        const penalty: number = await devolution.postDevolution(request.body.isn_id_cop, request.body as DevolutionModel);
         if (typeof penalty === "number" && penalty > 0)
           return {penalty};
         reply.status(200).send({penalty});
